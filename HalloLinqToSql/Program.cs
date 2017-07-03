40adf26e739f8845bd01be9faf77b94f2e459356
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-07-03 13:45:53 +0200

    Linq To Sql

---
+++ HalloLinqToSql/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace HalloLinqToSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new NorthwindClassesDataContext())
+            {
+                var employees = context.Employees.First();
+                employees.FirstName = "Stanislaus";
+                context.SubmitChanges();
+
+                //var sepp = new Employee { FirstName = "Sepp", LastName = "Huber" };
+                //context.Employees.InsertOnSubmit(sepp);
+                //context.SubmitChanges();
+
+
+                //var sepp = context.Employees.FirstOrDefault(e => e.FirstName == "Sepp" && e.LastName == "Huber");
+                //context.Employees.DeleteOnSubmit(sepp);
+                //context.SubmitChanges();
+            }
+        }
+    }
+}
