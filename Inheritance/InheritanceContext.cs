035a4f3d7a179df93fd88927a88aa229bbe27ce4
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-07-05 14:47:33 +0200

    Setup Inheritance

---
+++ Inheritance/InheritanceContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace Inheritance
+{
+    public class InheritanceContext : DbContext
+    {
+        public InheritanceContext() 
+            : base("Data Source=.;Initial Catalog=InheritanceDb;Integrated Security=True")
+        { }
+    }
+}
